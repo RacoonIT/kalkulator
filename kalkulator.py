229e7aa3d0d60e8314231d0ecf10f63f85c6a139
tlitwinski <tlitwinski@debian.tlitwinski>
2025-01-12 21:00:53 +0100

    Dodano prosty kalkulator

---
+++ kalkulator.py
@@ -0,0 +1,8 @@
+def kalkulator():
+	print("prosty kalkulator")
+	a = float(input("Podaj pierwsza liczbe: "))
+	b = float(input("Podaj druga liczbe: "))
+	print("Wynik dodawania:", a + b)
+
+if __name__ == "__main__":
+	kalkulator()
